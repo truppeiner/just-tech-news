60e3af2d855c0a3be14c2ab47b2ae50d95da5e83
Thomas Ruppeiner <truppeiner@gmail.com>
2022-05-03 16:44:15 -0500

    updated package.json and connection.js

--- config/connection.js
+++ config/connection.js
@@ -1,20 +1,17 @@
-// import the sequelize constructor from the library
 const Sequelize = require('sequelize');
 
 require('dotenv').config();
 
-// create connection to our database, pass in your MySQL information for USR PW
 let sequelize;
 
-if(process.env.JAWSDB_URL){
-    sequelize = new sequelize(process.env.JAWSDB_URL);
+if (process.env.JAWSDB_URL) {
+  sequelize = new Sequelize(process.env.JAWSDB_URL);
 } else {
-    sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
+  sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
     host: 'localhost',
     dialect: 'mysql',
     port: 3306
-});
+  });
 }
-
 
 module.exports = sequelize;
