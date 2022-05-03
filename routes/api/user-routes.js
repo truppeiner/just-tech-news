177a71c397f10840dd648f2456822625da07f198
Thomas Ruppeiner <truppeiner@gmail.com>
2022-05-03 15:35:15 -0500

    updated vote routes and modularized some code

--- routes/api/user-routes.js
+++ routes/api/user-routes.js
@@ -1,6 +1,6 @@
 const router = require('express').Router();
 
-const { User } = require('../../models');
+const { User, Post, Vote } = require('../../models');
 
 // GET api/users
 router.get('/', (req, res) =>{
@@ -18,6 +18,18 @@ router.get('/', (req, res) =>{
 // GET api/users/1
 router.get('/:id', (req, res) =>{
     User.findOne({
+      include: [
+      {
+        model: Post,
+        attributes: ['id', 'title', 'post_url', 'created_at']
+      },
+      {
+        model: Post,
+        attributes: ['title'],
+        through: Vote,
+        as: 'voted_posts'
+      }
+    ],
         attributes: {exclude: ['password'] },
         where: {
             id: req.params.id
