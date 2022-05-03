421ab72a50e0367108da2872ce126ae6927451f8
Thomas Ruppeiner <truppeiner@gmail.com>
2022-05-03 17:54:41 -0500

    added html homepage

--- controllers/index.js
+++ controllers/index.js
@@ -3,10 +3,13 @@ const router = require('express').Router();
 const userRoutes = require('./api/user-routes');
 const postRoutes = require('./api/post-routes');
 const commentRoutes = require('./api/comment-routes');
+const homeRoutes = require('./home-routes');
+
 
 router.use('/api/users', userRoutes);
 router.use('/api/posts', postRoutes);
 router.use('/api/comments', commentRoutes);
+router.use('/', homeRoutes);
 
 router.use((req, res) =>{
     res.status(404).end();
